456faa4850e603c0873c61b887eef8c7c022d02c
Ednilson Santana <ednilson.sdas@outlook.com>
2024-07-03 19:48:32 -0300

    Project created

---
+++ SalesWebMVC/Program.cs
@@ -0,0 +1,14 @@
+using System;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace SalesWebMvc
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+           
+        }
+    }
+}
