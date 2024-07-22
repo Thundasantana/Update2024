c92fdc95afcc58962b8a210534cdd8f34eab7ed3
Ednilson Santana <ednilson.sdas@outlook.com>
2024-07-22 20:43:19 -0300

    Seeding Service

---
+++ SalesWebMVC/Models/Seller.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesWebMvc.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+        public int Email { get; set; }
+        public DateTime BirthDate { get; set; }
+        public int BaseSalary { get; set; }
+        public Department Department { get; set; }
+        public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+
+        public Seller() { }
+
+        public Seller(int id, int name, int email, DateTime birthDate, int baseSalary, Department department)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            BirthDate = birthDate;
+            BaseSalary = baseSalary;
+            Department = department;
+        }
+
+        public void AddSales(SalesRecord sr)
+        {
+            Sales.Add(sr);
+        }
+
+        public void Removesales(SalesRecord sr)
+        {
+            Sales.Remove(sr);
+
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sales.Where(sr => sr.Date >= initial && sr.Date <= final).Sun(sr => sr.Amount);
+        }
+    }
+}
