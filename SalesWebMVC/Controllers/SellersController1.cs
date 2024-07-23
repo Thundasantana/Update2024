5380f95206f7929dc2dd09fb0825a8dc029b705c
Ednilson Santana <ednilson.sdas@outlook.com>
2024-07-22 21:01:09 -0300

    SellersController

---
+++ SalesWebMVC/Controllers/SellersController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SalesWebMvc.Controllers
+{
+    public class SellersController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
